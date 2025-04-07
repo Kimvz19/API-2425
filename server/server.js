1cf21eecf026515fd32c392c0b183b705da2044c
kim <Kimvz19@users.noreply.github.com>
2025-04-07 14:04:47 +0200

    fixing the code

--- server/server.js
+++ server/server.js
@@ -21,31 +21,20 @@ const engine = new Liquid({
   extname: '.liquid',
 });
 
-// 💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗
 
 // als de route klopt wordt de url geprint
 // server
 const app = new App();
 
 
-// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+
 // Variables for api fetching
-// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️ 
 // variables for .env files
 
 const apiKey = process.env.API_KEY;
 const apiSecret = process.env.API_SECRET;
 const baseUrl = process.env.BASE_URL;
 
-
-// 💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗💗
-
-
-// because i use a client server, i need to define the route 
-app
-  .use(logger())
-  .use('/', sirv(process.env.NODE_ENV === 'development' ? 'client' : 'dist'))
-  .listen(3000, () => console.log('Server available on http://localhost:3000'));
 
 
 
@@ -64,7 +53,7 @@ app.get('/', async (req, res) => {
       })
     });
 
-    // chancing answer to json
+    // changing answer to json
     const tokenData = await tokenResponse.json();
     const accessToken = tokenData.access_token;
 
@@ -78,38 +67,14 @@ app.get('/', async (req, res) => {
     const petfinderData = await petfinder.json();
     console.log(petfinderData);
 
-    // // render je Liquid template
-    // return res.send(renderTemplate('server/views/index.liquid', {
-    //   title: 'Home',
-    //   animals: petfinderData.animals
-    // }));
+    // render je Liquid template
+    return res.send(renderTemplate('server/views/index.liquid', { title: `hi`}));
 
-  // error notification
+    // error notification
   } catch (error) {
     console.error('Problem with fetching data:', error);
     res.status(500).send('Something went wrong');
   }
-});
-
-
-
-// const petfinder = await fetch('https://api.petfinder.com/v2/'+apiKey);
-//   const petfinder = await fetch(apiKey);
-//   const petfinderData = await petfinder.json();
-//   console.log(petfinderData);
-
-//   return res.send(renderTemplate('server/views/index.liquid', { title: 'Home', animals: petfinderData }));
-// });
-
-
-
-app.get('/pokemon/:name/', async (req, res) => {
-  // kan je iets opvragen
-  const name = req.params.name;
-  // voegt de data toe aan de url key
-  const petfinder = await fetch(apiKey + name);
-
-  return res.send(renderTemplate('server/views/detail.liquid', { title: `Detail page for ${id}`, item }));
 });
 
 
@@ -122,50 +87,8 @@ const renderTemplate = (template, data) => {
   return engine.renderFileSync(template, templateData);
 };
 
-
-// const apiUrl = 'https://dogapi.dog/api/v2/breeds';
-// app.get('/api/dogs', async (req, res) => {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const json = await response.json();
-//     res.json(json);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Fout bij ophalen van API-data' });
-//   }
-// });
-
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const json = await response.json();
-//     console.log('API Response:', json);
-//   } catch (error) {
-//     console.error('Fout bij ophalen van data:', error);
-//   }
-// };
-
-// fetchData();
-
-
-// const apiUrl = 'https://dogapi.dog/api/v2/breeds';
-// app.get('/api/dogs', async (req, res) => {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const json = await response.json();
-//     res.json(json);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Fout bij ophalen van API-data' });
-//   }
-// });
-
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const json = await response.json();
-//     console.log('API Response:', json);
-//   } catch (error) {
-//     console.error('Fout bij ophalen van data:', error);
-//   }
-// };
-
-// fetchData();
+// because i use a client server, i need to define the route 
+app
+  .use(logger())
+  .use('/', sirv(process.env.NODE_ENV === 'development' ? 'client' : 'dist'))
+  .listen(3000, () => console.log('Server available on http://localhost:3000'));
