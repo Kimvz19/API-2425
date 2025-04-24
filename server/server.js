59d92f10211a303c48c12bd31e5dc7d23181d2c4
kim <Kimvz19@users.noreply.github.com>
2025-04-24 08:18:58 +0200

    fixing gateway errorrrr

--- server/server.js
+++ server/server.js
@@ -254,4 +254,5 @@ app
   .use(logger())
   .use('/', sirv(process.env.NODE_ENV === 'development' ? 'client' : 'dist'))
   .use('/assets', sirv('assets'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(3000, () => console.log('Server running at http://localhost:3000'));
+  // .listen(PORT, () => console.log(`Listening on ${PORT}`));
